7bad80be3c539f5fbae5dd7295cc7008597036e0
Ayush Kumar Mishra <ayushmishra2005@gmail.com>
2020-11-23 10:38:14 +0530

    minor refactoring

--- runtime-api/src/lib.rs
+++ runtime-api/src/lib.rs
@@ -1,10 +1,10 @@
 #![cfg_attr(not(feature = "std"), no_std)]
-#![allow(clippy::too_many_arguments)]
-#![allow(clippy::unnecessary_mut_passed)]
 
 sp_api::decl_runtime_apis! {
+    /// The API to verify barcode
     pub trait VerifyBarcodeApi<Hash> where
 		Hash: codec::Codec, {
+		/// Verify barcode
         fn is_valid_barcode(barcode: Hash) -> bool;
     }
 }
